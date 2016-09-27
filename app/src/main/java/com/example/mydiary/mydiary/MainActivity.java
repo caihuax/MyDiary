27aab7367654a19aa76bc65d91b6819a56543104
caihuax <l>
2016-09-27 12:29:24 +0800

    almost

--- app/src/main/java/com/example/mydiary/mydiary/MainActivity.java
+++ app/src/main/java/com/example/mydiary/mydiary/MainActivity.java
@@ -5,21 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = Calendar.getInstance();
+        int y = calendar.get(Calendar.YEAR);
+        int m = calendar.get(Calendar.MONTH);
 
         Button new_diary = (Button) this.findViewById(R.id.plus_button);
         new_diary.setOnClickListener(new View.OnClickListener() {
@@ -29,16 +42,88 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(edit_intent);
             }
         });
+
+        Spinner month_spinner = (Spinner) this.findViewById(R.id.pick_month);
+        month_spinner.setSelection(m);
+        month_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //calendar.set(Calendar.MONTH,position);
+                //refreshDiaryList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Spinner year_spinner = (Spinner) this.findViewById(R.id.pick_year);
+        List<String> year_list = new ArrayList<String>();
+        y = y < 1970 ? 1970 : y;
+        for(int i = 1970;i <= y; ++i){
+            year_list.add(i+"");
+        }
+        ArrayAdapter<String> year_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, year_list);
+        year_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        year_spinner.setAdapter(year_adapter);
+        year_spinner.setSelection(y - 1970);
+        year_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //calendar.set(Calendar.YEAR,1970+position);
+                //refreshDiaryList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
-    protected void onStart(){
-        super.onStart();
+    protected void onResume(){
+        super.onResume();
 
+        refreshDiaryList();
+
+    }
+
+    protected  Object getDiary(String name){
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            fis = this.openFileInput(name);
+            ois = new ObjectInputStream(fis);
+            return ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //这里是读取文件产生异常
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {  //fis流关闭异常
+                    e.printStackTrace();
+                }
+            }
+            if (ois != null){
+                try {
+                    ois.close();
+                } catch (IOException e) {  //ois流关闭异常
+                    e.printStackTrace();
+                }
+            }
+        }  //读取产生异常，返回null
+        return new DiaryCopy();
+    }
+
+    protected void refreshDiaryList()
+    {
         LinearLayout diary_list = (LinearLayout) this.findViewById(R.id.diary_list);
         diary_list.removeAllViews();
 
-        Calendar calendar = Calendar.getInstance();
         int y = calendar.get(Calendar.YEAR);
         int m = calendar.get(Calendar.MONTH) + 1;  //得到的月份是0-11 所以要+1
         int d = Calculate.countDaysOfaMonth(calendar);  //这里的d是当前月份的天数
@@ -87,7 +172,8 @@ public class MainActivity extends AppCompatActivity {
 
                 TextView diary_text = new TextView(this);
                 diary_text.setText(diaryCopy.getText());
-                diary_text.setGravity(Gravity.CENTER);
+                diary_text.setGravity(Gravity.START);
+                diary_text.setPadding(14,14,14,14);
 
                 item.addView(textFrame);
                 item.addView(dateFrame);
@@ -103,36 +189,6 @@ public class MainActivity extends AppCompatActivity {
             }
             diary_list.addView(item);
         }
-
-    }
-
-    Object getDiary(String name){
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
-        try {
-            fis = this.openFileInput(name);
-            ois = new ObjectInputStream(fis);
-            return ois.readObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-            //这里是读取文件产生异常
-        } finally {
-            if (fis != null){
-                try {
-                    fis.close();
-                } catch (IOException e) {  //fis流关闭异常
-                    e.printStackTrace();
-                }
-            }
-            if (ois != null){
-                try {
-                    ois.close();
-                } catch (IOException e) {  //ois流关闭异常
-                    e.printStackTrace();
-                }
-            }
-        }  //读取产生异常，返回null
-        return new DiaryCopy();
     }
 
 }
